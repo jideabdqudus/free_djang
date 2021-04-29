eb38de73b91107161e9e7b5e156dbc80e3e3a6ad
jideabdqudus <jideabdqudus@gmail.com>
2021-04-29 20:29:13 +0100

    +

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view"""
+    def get(self, request, format=None):
+        """Returns a list of APIView Features"""
+        an_apiview = [
+            'GET, PUT, PATCH, DELETE, POST',
+            'Only 4 requests are popular tbh',
+            'Django is pretty cool'
+        ]
